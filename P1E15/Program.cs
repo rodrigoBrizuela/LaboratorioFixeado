d11be5ce379bf49d3210b4f3d056166bb26e43b5
NickAybar <nico.aybarc@gmail.com>
2018-04-26 00:46:15 -0300

    p1e15

--- P1E15/Program.cs
+++ P1E15/Program.cs
@@ -15,45 +15,44 @@ namespace P1E15
             decimal _pago = 0m;
             decimal _total = 0m;
             decimal _vuelto = 0m;
-            bool _bandera = false;
+            bool _bandera = false; // centinela
 
-            Console.Clear();
 
+            
             // operamos
             while (_bandera == false)
-                
+
             {
-                Console.WriteLine("Por favor, ingrese el total de la venta:");
+                Console.Clear(); // console clear va dentro del while 
+                Console.WriteLine("Por favor, ingrese el total de la venta: ");
                 if (decimal.TryParse(Console.ReadLine(), out _tarifa))
                 {
-                    Console.WriteLine("Por favor, ingrese el total pagado:");
+                    Console.WriteLine("Por favor, ingrese el total pagado: ");
                     if (decimal.TryParse(Console.ReadLine(), out _pago))
                     {
-                        if (_tarifa > _pago)
+                        if (_tarifa > _pago) // condicion para el descuento
                         {
-                                _total = (100 - ((_pago * 100) / _tarifa));
+                            _total = (100 - ((_pago * 100) / _tarifa));
                             Console.WriteLine($"Usted obtuvo un descuento del {_total}%");
                             _bandera = true;
-                            if (_tarifa < _pago)
-                            {
-                                _vuelto = (_pago - _tarifa);
-                                Console.WriteLine($"El monto pagado es mayor al total de venta. Su vuelto es de {_vuelto}");
-                            }
+                        }
+                        else if (_tarifa < _pago) // condicion para el vuelto
+                        {
+                            _vuelto = (_pago - _tarifa);
+                            Console.WriteLine($"El monto pagado es mayor al total de venta. Su vuelto es de {_vuelto}");
                         }
                     }
                     else
                     {
-                        Console.WriteLine(" Por favor ingrese un caracter valido.\n Presione cualquier tecla para reiniciar");
+                        Console.WriteLine("Por favor ingrese un caracter valido.\nPresione cualquier tecla para reiniciar.");
                     }
                 }
-
                 else
                 {
-                    Console.WriteLine("Por favor ingrese un caracter valido.\n Presione cualquier tecla para reiniciar");
+                    Console.WriteLine("Por favor ingrese un caracter valido.\nPresione cualquier tecla para reiniciar.");
                 }
                 Console.ReadKey();
             }
         }
-
     }
 }

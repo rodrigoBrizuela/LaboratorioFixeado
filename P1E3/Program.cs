4a7f6a70b0be63349f4ce811eee4736904e088ee
NickAybar <nico.aybarc@gmail.com>
2018-05-03 22:12:31 -0300

    Ejercicio 3 Finalizado

--- P1E3/Program.cs
+++ P1E3/Program.cs
@@ -8,42 +8,60 @@ namespace P1E3
 {
     class Program
     {
+        // Tecnicatura Superior en Programacion
+        // Comision 2
+        // Aybar Critto, Nicolas Exequiel
+        // Ejercicio 3
+
         static void Main(string[] args)
         {
             //Declarar variables
             decimal _numero = 0m;
 
-            Console.Clear();
+            // Declaracion while
+            string _deseaCalcular = "S";
+
+
             // Operar
 
-            Console.Write("Ingrese un numero: ");
-            if (decimal.TryParse(Console.ReadLine(), out _numero))
+            while (_deseaCalcular.ToUpper() == "S")
             {
-               if (_numero == 0)
+                Console.Clear();
+                Console.WriteLine("Ingrese un numero: ");
+                if (decimal.TryParse(Console.ReadLine(), out _numero))
                 {
-                    Console.WriteLine("El numero ingresado es neutro");
-                }
-                {
-                    if (_numero > 0)
+                    if (_numero == 0)
                     {
-                        Console.WriteLine("El numero ingresado es positivo");
+                        Console.WriteLine("El numero ingresado es neutro");
                     }
-                    { if (_numero < 0)
+                    {
+                        if (_numero > 0)
                         {
-                            Console.WriteLine("El numero ingresado es negativo");
+                            Console.WriteLine("El numero ingresado es positivo");
                         }
-                       
-                        
+                        {
+                            if (_numero < 0)
+                            {
+                                Console.WriteLine("El numero ingresado es negativo");
+                            }
+
+
+                        }
                     }
+
                 }
+                else
+                {
+                    Console.WriteLine("Ocurrio un error. Por favor ingrese un caracter numerico.");
 
-            }
-            else
-            {
-                Console.WriteLine("Ocurrio un error. Por favor ingrese un caracter numerico.");
-
+                }
+                Console.WriteLine();
+                Console.WriteLine("Desea calcular otro numero? [S para SI - N para NO]");
+                _deseaCalcular = Console.ReadLine();
+                Console.WriteLine("Presione cualquier tecla para finalizar..."); 
             }
             Console.ReadKey();
         }
+       
     }
 }

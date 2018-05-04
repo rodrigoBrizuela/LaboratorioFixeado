70377c1a8840d0f6f18c8fb7cc18aa220eba0543
NickAybar <nico.aybarc@gmail.com>
2018-05-03 22:38:33 -0300

    Ejercicio 8 finalizado

--- P1E8/Program.cs
+++ P1E8/Program.cs
@@ -8,6 +8,11 @@ namespace P1E8
 {
     class Program
     {
+        // Tecnicatura Superior en Programacion
+        // Comision 2
+        // Aybar Critto, Nicolas Exequiel
+        // Ejercicio 8
+
         static void Main(string[] args)
         {
             // DECLARAR
@@ -17,16 +22,21 @@ namespace P1E8
 
             // Operamos
             Console.WriteLine("Listado numeros impares del 1 al 999");
+            Console.WriteLine("Presione cualquier tecla para mostrar los numeros impares.");
+            Console.ReadKey();
             for (_impar = 1; _impar <= 999; _impar = _impar + 2)
             {
                 Console.WriteLine(_impar.ToString("000"));
             }
-
+            Console.WriteLine();
             Console.WriteLine("Listado numeros pares del 2 al 1000");
+            Console.WriteLine("Presione cualquier tecla para mostrar los numeros pares.");
+            Console.ReadKey();
             for (_par = 2; _par <= 1000; _par = _par + 2)
             {
                 Console.WriteLine(_par.ToString("000"));
             }
+            Console.WriteLine("Presione cualquier tecla para finalizar.");
             Console.ReadKey();
         }
     }

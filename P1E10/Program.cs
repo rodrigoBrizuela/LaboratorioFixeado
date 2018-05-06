ed7dc2e0cee85f636b48a65a6f19151d07308173
NickAybar <nico.aybarc@gmail.com>
2018-05-05 21:42:46 -0300

    Ejercicio 11 terminado

--- P1E10/Program.cs
+++ P1E10/Program.cs
@@ -8,26 +8,48 @@ namespace P1E10
 {
     class Program
     {
+        // Tecnicatura Superior en Programacion
+        // Comision 2
+        // Aybar Critto, Nicolas Exequiel
+        // Ejercicio 10
+
         static void Main(string[] args)
         {
-            //declarar
+            //almacenamiento de los valores mayor menor
             int _mayor = 0;
             int _menor = 0;
+            //
             int _numero = 0;
-            int _n = 0;
+            Random _generador = new Random(); // Generador de numeros aleatorios
 
             Console.Clear();
 
             Console.WriteLine("Numeros a ingresar: ");
-            if(int.TryParse(Console.ReadLine(), out _n))
+
+            for (int _i = 1; _i <= 20; _i++)
             {
-                for (int _i = 1; _i <= _n; _i++)
+                _numero = _generador.Next(100);
+                Console.WriteLine(_numero.ToString("00"));
+                if(_numero > _mayor)
                 {
-                    Console.Write("Primer numero {0}: ");
-                    if(int.TryParse(Console.ReadLine(), out _numero))
+                    if(_mayor > _menor)
+                    {
+                        _menor = _mayor;
+                        _mayor = _numero;
+                    }
+                    else
+                    {
+                        _mayor = _numero;
+                    }
+                }
+                else if (_numero >= _menor && _numero < _mayor)
+                {
+                    _menor = _numero;
+                
                 }
             }
-
+            Console.WriteLine($"El numero mayor es {_mayor} y el menor es {_menor}");
+            Console.ReadKey();
         }
     }
 }
--- P1E11/Program.cs
+++ P1E11/Program.cs
@@ -9,6 +9,7 @@ namespace P1E11
     class Program
     {
         // Tecnicatura Superior en Programacion
+        // Comision 2
         // Aybar Critto, Nicolas Exequiel
         // Ejercicio 11
 
@@ -16,42 +17,44 @@ namespace P1E11
         static void Main(string[] args)
         {
             //declarar
-            int _x = 0; // numero base
-            int _n = 0; // exponente
-            int _y = 0; // resultado
-            bool _bandera = false; //  bandera while
+            double _x = 0; // numero base
+            double _n = 0; // exponente
+            double _resultado = 0; // resultado
 
-            Console.Clear();
+            //declaracion while
+            string _deseaCalcular = "S";
 
-            while (_bandera == false)
+     
+
+            while (_deseaCalcular.ToUpper() == "S")
             {
+                Console.Clear();
                 Console.WriteLine("Ingrese un numero: ");
-                if (int.TryParse(Console.ReadLine(), out _x))
+                if (double.TryParse(Console.ReadLine(), out _x))
                 {
                     Console.WriteLine("Ingrese el exponente al que desea elevar el numero:");
                     {
-                        if (int.TryParse(Console.ReadLine(), out _n))
+                        if (double.TryParse(Console.ReadLine(), out _n))
                         {
-                            _y = 1; // El resultado no puede ser 0, esto ayuda a que un numero elevado a la 0 es = a 1
-                            for (int _i = 1; _i <= _n; _i++) // declaro la variable de inicializacion int _i para el for
-                            {
-                                _y = (_x * _y); // formula para obtener el exponente ( Resultado = base x resultado )
-                            }
-                            Console.WriteLine($"El numero {_x} elevado a la {_n} es igual a {_y}");
-                            _bandera = true;
+                            _resultado = Math.Pow(_x, _n);
+                            Console.WriteLine($"El numero {_x} elevado a la {_n} es igual a {_resultado}");
                         }
                         else
                         {
-                            Console.WriteLine("Por favor ingrese un caracter valido.");
+                            Console.WriteLine("Ocurrio un error. Por favor ingrese un caracter valido.");
                         }
 
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Por favor ingrese un caracter valido.");
-
+                    Console.WriteLine("Ocurrio un error. Por favor ingrese un caracter valido.");
                 }
+
+                Console.WriteLine();
+                Console.WriteLine("Desea calcular otro numero? [S para SI - N para NO]");
+                _deseaCalcular = Console.ReadLine();
+                Console.WriteLine("Presione cualquier tecla para finalizar...");
             }
             Console.ReadKey();
         }

33cbc6265258e5299c2bae06c39e1bf1db076921
NickAybar <nico.aybarc@gmail.com>
2018-05-05 22:03:00 -0300

    Ejercicio 13 finalizado

--- P1E13/Program.cs
+++ P1E13/Program.cs
@@ -8,68 +8,84 @@ namespace P1E13
 {
     class Program
     {
+        // Tecnicatura Superior en Programacion
+        // Comision 2
+        // Aybar Critto, Nicolas Exequiel
+        // Ejercicio 13
+
         static void Main(string[] args)
         {
-            // Capturar los valores ingresados por el usuario
-            Console.Write("Ingrese la primer nota: ");
-            string _notaStr1 = Console.ReadLine();
-            Console.Write("Ingrese la segunda nota: ");
-            string _notaStr2 = Console.ReadLine();
-            Console.Write("Ingrese la tercera nota: ");
-            string _notaStr3 = Console.ReadLine();
-           
+            // DECLARAR VALORES
 
-            decimal _nota1 = 0m; // se escribe el 0m; para determinar que el 0 tiene decimales.
+            decimal _nota1 = 0m;
             decimal _nota2 = 0m;
             decimal _nota3 = 0m;
             decimal _promedio = 0m;
 
-            if (decimal.TryParse(_notaStr1, out _nota1))
+            // Condicion while
+            string _deseaCalcular = "S";
+
+            // OPERAMOS
+            
+            while(_deseaCalcular.ToUpper() == "S")
             {
-                if (decimal.TryParse(_notaStr2, out _nota2))
+                Console.Clear();
+                Console.WriteLine("Por favor ingrese la primera nota:");
+                if (decimal.TryParse(Console.ReadLine(), out _nota1))
                 {
-                    if (decimal.TryParse(_notaStr3, out _nota3))
+                    Console.WriteLine("Por favor ingrese la segunda nota:");
+                    if (decimal.TryParse(Console.ReadLine(), out _nota2))
                     {
-                        // Aqui opero, hago los 3 if uno dentro de otro, porque tiene que pasar las tres validaciones.
-                        // Si no pasa una validacion, se deriva directamente al else correspondiente
-                        Console.WriteLine(); // Deja un espacio en blanco. Salto de linea. 
+                        Console.WriteLine("Por favor ingrese la tercera nota:");
+                        if (decimal.TryParse(Console.ReadLine(), out _nota3))
+                        {
+                            // Aqui opero, hago los 3 if uno dentro de otro, porque tiene que pasar las tres validaciones.
+                            // Si no pasa una validacion, se deriva directamente al else correspondiente
+                            Console.WriteLine(); // Deja un espacio en blanco. Salto de linea. 
+                            _promedio = (_nota1 + _nota2 + _nota3) / 3;
 
-                        _promedio = (_nota1 + _nota2 + _nota3) / 3;
-
-                        if (_promedio == 10)
-                        {
-                            Console.WriteLine("Excelente");
+                            if (_promedio > 10)
+                            {
+                                Console.WriteLine("El promedio no puede ser mayor a 10. Por favor revise las notas e ingreselas nuevamente.");
+                            }
+                            if (_promedio == 10)
+                            {
+                                Console.WriteLine("Excelente");
+                            }
+                            if (_promedio >= 7 && _promedio < 10)
+                            {
+                                Console.WriteLine("Muy bueno");
+                            }
+                            if (_promedio >= 4 && _promedio < 7)
+                            {
+                                Console.WriteLine("Bueno");
+                            }
+                            if (_promedio >= 0 && _promedio < 4)
+                            {
+                                Console.WriteLine("Regular");
+                            }
                         }
-                        if(_promedio >= 7 && _promedio < 10)
+                        else
                         {
-                            Console.WriteLine("Muy bueno");
-                        }
-                        if (_promedio >= 4 && _promedio < 7)
-                        {
-                            Console.WriteLine("Bueno");
-                        }
-                        if (_promedio >= 0 && _promedio < 4)
-                        {
-                            Console.WriteLine("Regular");
+                            Console.WriteLine("Ocurrio un error al ingresar la tercera nota.");
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Ocurrio un error al ingresar la tercera nota.");
+                        Console.WriteLine("Ocurrio un error al ingresar la segunda nota.");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Ocurrio un error al ingresar la segunda nota.");
+                    Console.WriteLine("Ocurrio un error al ingresar la primera nota.");
+
                 }
-            }
-            else
-            {
-                Console.WriteLine("Ocurrio un error al ingresar la primera nota.");
-
-            }
+                Console.WriteLine();
+                Console.WriteLine("Desea calcular otro promedio? [S para SI - N para NO]");
+                _deseaCalcular = Console.ReadLine();
+                Console.WriteLine("Presione cualquier tecla para finalizar...");
+            }      
              Console.ReadKey(); // Lee lo que ingresa el usuario
-
         }
     }
 }

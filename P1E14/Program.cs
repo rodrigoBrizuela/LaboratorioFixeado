e8f5fe6706b5e21a71dd025463489c27d47d5e15
NickAybar <nico.aybarc@gmail.com>
2018-05-05 22:09:09 -0300

    ejercicio 14 terminado

--- P1E14/Program.cs
+++ P1E14/Program.cs
@@ -8,23 +8,44 @@ namespace P1E14
 {
     class Program
     {
+        // Tecnicatura Superior en Programacion
+        // Comision 2
+        // Aybar Critto, Nicolas Exequiel
+        // Ejercicio 14
+
         static void Main(string[] args)
         {
             //DECLARAR
             decimal _millas = 0m;
             decimal _metros = 0m;
+            decimal _conversor = 1852m;
 
-            Console.Clear();
+            // operacion while
+            string _deseaCalcular = "S";
 
             // Operar
-            Console.WriteLine("Ingrese la cantidad de millas marinas que desea convertir a metros.");
-            if (decimal.TryParse(Console.ReadLine(), out _millas))
-                
+            while(_deseaCalcular.ToUpper() == "S")
             {
-                _metros = (_millas * 1852);
-                Console.WriteLine($"La cantidad de millas marinas ingresadas equivale a {_metros} metros");
-            }
+                Console.Clear();
 
+
+                Console.WriteLine("Ingrese la cantidad de millas marinas que desea convertir a metros.");
+                if (decimal.TryParse(Console.ReadLine(), out _millas))
+
+                {
+                    _metros = (_millas * _conversor);
+                    Console.WriteLine($"La cantidad de millas marinas ingresadas equivale a {_metros} metros");
+                }
+                else
+                {
+                    Console.WriteLine("Ocurrio un error. Por favor ingrese un numero valido.");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Desea convertir otro numero? [S para SI - N para NO]");
+                _deseaCalcular = Console.ReadLine();
+                Console.WriteLine("Presione cualquier tecla para finalizar...");
+            }
             Console.ReadKey();
         }
     }
